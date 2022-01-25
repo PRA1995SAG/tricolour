e5ea8b823f84944a07f331f4b3a9eedaba7509cd
PRA1995SAG <livethefree@gmail.com>
2022-01-25 22:51:47 +0530

    Add: version one

--- App.js
+++ App.js
@@ -8,97 +8,113 @@
 
 import React from 'react';
 import {StyleSheet, View} from 'react-native';
+import getDegree from './util';
 
 const Spoke = ({index}) => {
-  let deg;
-  const DISTANCE = 15;
-  switch (index) {
-    case '0':
-      deg = `${DISTANCE * index}deg`;
-      break;
-    case '1':
-      deg = `${DISTANCE * index}deg`;
-      break;
-    case '2':
-      deg = `${DISTANCE * index}deg`;
-      break;
-    case '3':
-      deg = `${DISTANCE * index}deg`;
-      break;
-    case '4':
-      deg = `${DISTANCE * index}deg`;
-      break;
-    case '5':
-      deg = `${DISTANCE * index}deg`;
-      break;
-    case '6':
-      deg = `${DISTANCE * index}deg`;
-      break;
-    case '7':
-      deg = `${DISTANCE * index}deg`;
-      break;
-    case '8':
-      deg = `${DISTANCE * index}deg`;
-      break;
-    case '9':
-      deg = `${DISTANCE * index}deg`;
-      break;
-    case '10':
-      deg = `${DISTANCE * index}deg`;
-      break;
-    case '11':
-      deg = `${DISTANCE * index}deg`;
-      break;
-    default:
-      deg = '0deg';
-  }
+  const deg = getDegree(index);
 
-  const position = {
-    position: 'absolute',
-    transform: [{rotate: deg}],
-  };
-  return <View style={[styles.spoke, position]} />;
+  return (
+    <View
+      style={{
+        position: 'absolute',
+        height: 0,
+        width: 0,
+        transform: [{rotate: deg}],
+      }}>
+      <View style={styles.head} />
+      <View style={styles.tail} />
+    </View>
+  );
 };
 
-const Center = () => {
+const Circle = ({children}) => {
+  return <View style={styles.circle}>{children}</View>;
+};
+
+const Saffron = () => {
+  return <View style={StyleSheet.flatten([styles.flagPart, styles.saffron])} />;
+};
+
+const Green = () => {
+  return <View style={StyleSheet.flatten([styles.flagPart, styles.green])} />;
+};
+
+const White = ({children}) => {
+  return (
+    <View style={StyleSheet.flatten([styles.flagPart, styles.white])}>
+      {children}
+    </View>
+  );
+};
+
+const TriColor = () => {
+  return (
+    <View style={styles.container}>
+      <Saffron />
+      <White>
+        <Chakra />
+      </White>
+      <Green />
+    </View>
+  );
+};
+
+const Chakra = () => {
   return (
     <View style={styles.center}>
-      <Spoke index="0" />
-      <Spoke index="1" />
-      <Spoke index="2" />
-      <Spoke index="3" />
-      <Spoke index="4" />
-      <Spoke index="5" />
-      <Spoke index="6" />
-      <Spoke index="7" />
-      <Spoke index="8" />
-      <Spoke index="9" />
-      <Spoke index="10" />
-      <Spoke index="11" />
+      <Circle>
+        <Spoke index="0" />
+        <Spoke index="1" />
+        <Spoke index="2" />
+        <Spoke index="3" />
+        <Spoke index="4" />
+        <Spoke index="5" />
+        <Spoke index="6" />
+        <Spoke index="7" />
+        <Spoke index="8" />
+        <Spoke index="9" />
+        <Spoke index="10" />
+        <Spoke index="11" />
+        <Spoke index="12" />
+        <Spoke index="13" />
+        <Spoke index="14" />
+        <Spoke index="15" />
+        <Spoke index="16" />
+        <Spoke index="17" />
+        <Spoke index="18" />
+        <Spoke index="19" />
+        <Spoke index="20" />
+        <Spoke index="21" />
+        <Spoke index="22" />
+        <Spoke index="23" />
+      </Circle>
     </View>
   );
 };
 
 const App = () => {
-  return (
-    <View style={styles.container}>
-      <View style={StyleSheet.flatten([styles.flagPart, styles.saffron])} />
-      <View style={StyleSheet.flatten([styles.flagPart, styles.white])}>
-        <Center />
-      </View>
-      <View style={StyleSheet.flatten([styles.flagPart, styles.green])} />
-    </View>
-  );
+  return <TriColor />;
 };
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    // paddingT`op: StatusBar.currentHeight,
     backgroundColor: '#fff',
   },
   flagPart: {
     flex: 0.3333,
+  },
+  head: {
+    width: 0,
+    height: 0,
+    borderStyle: 'solid',
+    backgroundColor: 'transparent',
+    borderLeftWidth: 8,
+    borderRightWidth: 8,
+    borderLeftColor: 'transparent',
+    borderRightColor: 'transparent',
+    borderBottomWidth: 8 * 2,
+    borderBottomColor: '#000080',
   },
   spoke: {
     height: 180,
@@ -118,6 +134,29 @@ const styles = StyleSheet.create({
   },
   green: {
     backgroundColor: '#138808',
+  },
+  circle: {
+    height: 180,
+    width: 180,
+    borderRadius: 180 / 2,
+    backgroundColor: 'transparent',
+    borderWidth: 8,
+    borderColor: '#000080',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  tail: {
+    width: 0,
+    height: 0,
+    borderStyle: 'solid',
+    backgroundColor: 'transparent',
+    borderLeftWidth: 8,
+    borderRightWidth: 8,
+    borderLeftColor: 'transparent',
+    borderRightColor: 'transparent',
+    borderBottomWidth: 16 * 4,
+    borderBottomColor: '#000080',
+    transform: [{rotate: '180deg'}],
   },
 });
 
---
+++ util.js
@@ -0,0 +1,57 @@
+export default function getDegree(index) {
+  const DISTANCE = 15;
+  switch (index) {
+    case '0':
+      return `${DISTANCE * index}deg`;
+    case '1':
+      return `${DISTANCE * index}deg`;
+    case '2':
+      return `${DISTANCE * index}deg`;
+    case '3':
+      return `${DISTANCE * index}deg`;
+    case '4':
+      return `${DISTANCE * index}deg`;
+    case '5':
+      return `${DISTANCE * index}deg`;
+    case '6':
+      return `${DISTANCE * index}deg`;
+    case '7':
+      return `${DISTANCE * index}deg`;
+    case '8':
+      return `${DISTANCE * index}deg`;
+    case '9':
+      return `${DISTANCE * index}deg`;
+    case '10':
+      return `${DISTANCE * index}deg`;
+    case '11':
+      return `${DISTANCE * index}deg`;
+    case '11':
+      return `${DISTANCE * index}deg`;
+    case '12':
+      return `${DISTANCE * index}deg`;
+    case '13':
+      return `${DISTANCE * index}deg`;
+    case '14':
+      return `${DISTANCE * index}deg`;
+    case '15':
+      return `${DISTANCE * index}deg`;
+    case '16':
+      return `${DISTANCE * index}deg`;
+    case '17':
+      return `${DISTANCE * index}deg`;
+    case '18':
+      return `${DISTANCE * index}deg`;
+    case '19':
+      return `${DISTANCE * index}deg`;
+    case '20':
+      return `${DISTANCE * index}deg`;
+    case '21':
+      return `${DISTANCE * index}deg`;
+    case '22':
+      return `${DISTANCE * index}deg`;
+    case '23':
+      return `${DISTANCE * index}deg`;
+    default:
+      return '0deg';
+  }
+}
